4059ab0ac31caf2d86b2b0d45fb31cec9d44ce05
anipakerimbekova <ani.kerimbekova@mail.ru>
2021-11-01 15:21:15 +0600

    Initial commit

---
+++ app/src/main/java/com/geektech/taskapp36/ProfileFragment.java
@@ -0,0 +1,54 @@
+package com.geektech.taskapp36;
+
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.geektech.taskapp36.databinding.FragmentHomeBinding;
+import com.geektech.taskapp36.databinding.FragmentProfileBinding;
+
+import org.jetbrains.annotations.NotNull;
+
+public class ProfileFragment extends Fragment {
+
+    private FragmentProfileBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        return root;
+
+    }
+
+    private ActivityResultLauncher<String> mGetContent = registerForActivityResult(new ActivityResultContracts.GetContent(),
+            new ActivityResultCallback<Uri>() {
+                @Override
+                public void onActivityResult(Uri uri) {
+                    binding.imageView.setImageURI(uri);
+                }
+            });
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.imageView.setOnClickListener(v -> {
+            mGetContent.launch("image/*");
+        });
+    }
+}
+
+
